ba14c66ce954d06abe5ecf8478c1f42d4997c5e2
Wilfredo Armando <wilfredoacreyes@outlook.com>
2023-03-22 22:36:37 -0600

    segundo ejer de primera pract del 1602

--- Codigos Arduino/Practica1,1_1602.ino
+++ Codigos Arduino/Practica1,1_1602.ino
@@ -1,5 +1,13 @@
-// C++ code
-//
+/*
+Unas luces navideñas modernas son las siguientes: 
+mientras se mantenga pulsado S1 una variable entera inicializada en 0 
+se comienza a incrementar de 20 en 20, al soltar S1 la variable para de incrementarse, 
+cuando se pulse S2 el valor de la variable se debe cargar a un ―Hola Mundo del LED, 
+esto quiere decir que   el LED va a estar intermitente en intervalos de tiempo iguales 
+al valor de la variable. Por consola serial debes ir mostrando el valor de la variable. 
+Para que puedas volver la variable a 0 y puedas hacer otra rutina de intermitencia, 
+coloca la condición de que S1 y S2 se opriman al tiempo, con ello la variable entera debe volver a 0.
+*/
 int p1 = 2;
 int p2 = 3;
 int led = 13;
